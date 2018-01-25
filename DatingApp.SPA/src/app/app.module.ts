4292c260b964d9cab20aa4b31c7c8acc9360f651
syonoki <syonoki@gmail.com>
2018-01-25 17:37:35 +0900

    on 4.39

--- DatingApp.SPA/src/app/app.module.ts
+++ DatingApp.SPA/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpModule  } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ValueComponent
+,
+    NavComponent
 ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpModule
   ],
   providers: [],
